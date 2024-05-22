4acce5938104314980fc1dff2f500c66f9d95aaa
GitHub Action <action@github.com>
2024-05-22 00:49:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/mrcheeze446/jetbrainsdcclasssupport/MyBundle.kt
+++ src/main/kotlin/com/github/mrcheeze446/jetbrainsdcclasssupport/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mrcheeze446.jetbrainsdcclasssupport
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mrcheeze446/jetbrainsdcclasssupport/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/mrcheeze446/jetbrainsdcclasssupport/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.mrcheeze446.jetbrainsdcclasssupport.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.mrcheeze446.jetbrainsdcclasssupport.MyBundle
+import com.github.mrcheeze446.jetbrainsdcclasssupport.services.MyProjectService
 import javax.swing.JButton
 
 
